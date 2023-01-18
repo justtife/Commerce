e4f548d084789a874b1c1e71928e43d6a4a22601
Justtife <farinubolu@gmail.com>
2023-01-18 19:07:03 +0100

    Error Index

---
+++ src/utils/errors/index.ts
@@ -0,0 +1,13 @@
+import { BadRequestError } from "./badRequest";
+import { ForbiddenError } from "./forbidden";
+import { NotFoundError } from "./notFound";
+import { UnauthorisedError } from "./unauthorised";
+import { CustomError } from "./custom";
+
+export default {
+  BadRequestError,
+  ForbiddenError,
+  NotFoundError,
+  UnauthorisedError,
+  CustomError,
+};
